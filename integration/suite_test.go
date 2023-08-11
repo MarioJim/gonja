286ec0fb1703010a28298a8a5364f87d9657a23e
MarioJim <mario.emilio.j@gmail.com>
2023-08-10 23:42:48 -0400

    Add flag for setting the root folder of the test data

--- integration/suite_test.go
+++ integration/suite_test.go
@@ -1,43 +1,47 @@
 package integration_test
 
 import (
+	"flag"
+	"fmt"
 	"testing"
 )
 
+var testdataFlag = flag.String("testdata", "./testdata", "root folder for the test data")
+
 func TestTemplates(t *testing.T) {
 	// Add a global to the default set
-	root := "./testdata"
+	root := *testdataFlag
 	env := testEnv(root)
 	env.Globals.Set("this_is_a_global_variable", "this is a global text")
 	GlobTemplateTests(t, root, env)
 }
 
 func TestExpressions(t *testing.T) {
-	root := "./testdata/expressions"
+	root := fmt.Sprintf("%s/expressions", *testdataFlag)
 	env := testEnv(root)
 	GlobTemplateTests(t, root, env)
 }
 
 func TestFilters(t *testing.T) {
-	root := "./testdata/filters"
+	root := fmt.Sprintf("%s/filters", *testdataFlag)
 	env := testEnv(root)
 	GlobTemplateTests(t, root, env)
 }
 
 func TestFunctions(t *testing.T) {
-	root := "./testdata/functions"
+	root := fmt.Sprintf("%s/functions", *testdataFlag)
 	env := testEnv(root)
 	GlobTemplateTests(t, root, env)
 }
 
 func TestTests(t *testing.T) {
-	root := "./testdata/tests"
+	root := fmt.Sprintf("%s/tests", *testdataFlag)
 	env := testEnv(root)
 	GlobTemplateTests(t, root, env)
 }
 
 func TestStatements(t *testing.T) {
-	root := "./testdata/statements"
+	root := fmt.Sprintf("%s/statements", *testdataFlag)
 	env := testEnv(root)
 	GlobTemplateTests(t, root, env)
 }
